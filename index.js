9d6606e5a34af568f436440388c63a1bce57e29c
vstura <ville.s.tura@gmail.com>
2022-03-08 09:47:56 +0200

    uibuild

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-
+const Person = require('./models/person');
 const app = express();
+
+// otetaan cors käyttöön
 app.use(cors());
-
-// json-parser
-app.use(express.json());
-
 // Etsitään build-tiedostoa
 app.use(express.static('build'));
+// json-parser
+app.use(express.json());
 
 // morgan middleware
 morgan.token('person', (req) => {
@@ -29,35 +29,19 @@ app.use(
   )
 );
 
-// persons-taulukko
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122',
-  },
-];
-
-const Person = require('./models/person');
-
 // Näytetään etusivu
 app.get('/', (req, res) => {
   res.send('<h1>It is me, the puhelinluettelo!</h1>');
+});
+
+// Näytetään ../info-sivu
+app.get('/info', (req, res) => {
+  Person.find({}).then((result) => {
+    res.send(
+      `<p>Phonebook has info for ${result.length} people</p>
+       <p>${new Date()}</p>`
+    );
+  });
 });
 
 // Näytetään ../persons-sivu
@@ -67,59 +51,92 @@ app.get('/api/persons', (req, res) => {
   });
 });
 
-// Näytetään ../info-sivu
-app.get('/info', (req, res) => {
-  res.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>`
-  );
-});
-
 // Näytetään yksittäinen resurssi
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then((person) => {
-    res.json(person);
-  });
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) res.json(person);
+      if (!person) res.status(400).end();
+    })
+    .catch((err) => next(err));
 });
 
 // Poistetaan yksittäinen resurssi
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  res.status(204).end();
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch((err) => next(err));
 });
 
 // Lisätään yksittäinen resurssi
-// const generateId = () => Math.floor(Math.random() * 10_000);
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body;
-  // const nameExists = persons.find(
-  //   (p) => p.name.toLowerCase() === body.name.toLowerCase()
-  // );
 
   if (!body.name) return res.status(400).json({ error: 'Name is missing' });
 
   if (!body.number) return res.status(400).json({ error: 'Number is missing' });
 
-  // if (nameExists) return res.status(400).json({ error: 'Name must be unique' });
-
   const person = new Person({
-    // id: generateId(),
     name: body.name,
     number: body.number,
   });
 
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((err) => next(err));
 });
 
+// RESURSSIN MUOKKAAMINEN (HTTP PUT)
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body;
+
+  Person.findByIdAndUpdate(
+    req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((updatedPerson) => {
+      res.json(updatedPerson);
+    })
+    .catch((err) => next(err));
+});
+
+// Virheiden käsittely middleware
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+// Olemattomien osoitteiden käsittely
+app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+  console.log(err.message);
+  if (err.name === 'CastError')
+    return res.status(400).send({ error: 'malformatted id' });
+
+  if (err.name === 'ValidationError')
+    return res.status(400).json({ error: err.message });
+
+  next(err);
+};
+
+// Virheellisten pyyntöjen käsittely. T
+app.use(errorHandler);
+
 // Palvelin kuittaa
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 // TEHTY
-// 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 3.10, 3.11, 3.12, 3.13, 3.14
+// 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 3.10, 3.11, 3.12, 3.13, 3.14, 3.15, 3.16, 3.17, 3.18, 3.19
--- models/person.js
+++ models/person.js
@@ -11,9 +11,26 @@ mongoose
   .catch((err) => {
     console.log(`error connecting to MongoDB: ${err.message}`);
   });
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function (number) {
+        return /^((\d{3}-\d{4,15})|(\d{2}-\d{5,15}))$/.test(number);
+      },
+      message: ({ value }) =>
+        `${value} is not a valid phone number! 
+        Number should start with either DD- or DDD-`,
+    },
+  },
 });
 
 personSchema.set('toJSON', {
